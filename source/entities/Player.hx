866c743a64d8b9ff2259d9ea00febdbd2bc7f788
Toby Alden <toby.alden@gmail.com>
2019-07-28 16:28:55 -0700

    Player rotates around attached hook

--- source/entities/Player.hx
+++ source/entities/Player.hx
@@ -20,6 +20,7 @@ class Player extends Entity
 
     public function new() {
         super(25, 300);
+        layer = -1;
         Key.define("left", [Key.LEFT, Key.LEFT_SQUARE_BRACKET]);
         Key.define("right", [Key.RIGHT, Key.RIGHT_SQUARE_BRACKET]);
         Key.define("jump", [Key.Z]);
@@ -74,13 +75,43 @@ class Player extends Entity
             }
         }
 
-        moveBy(velocity.x * HXP.elapsed, velocity.y * HXP.elapsed, "walls");
+        if(hook != null && hook.isAttached) {
+            // https://math.stackexchange.com/questions/814950/how-can-i-rotate-a-coordinate-around-a-circle
+            var rotateAmount = 0.05;
+            var xRot = (
+                Math.cos(rotateAmount) * (centerX - hook.centerX)
+                - Math.sin(rotateAmount) * (centerY - hook.centerY)
+                + hook.centerX
+            );
+            var yRot = (
+                Math.sin(rotateAmount) * (centerX - hook.centerX)
+                + Math.cos(rotateAmount) * (centerY - hook.centerY)
+                + hook.centerY
+            );
+            x = xRot - width / 2;
+            y = yRot - height / 2;
+        }
+        else {
+            moveBy(
+                velocity.x * HXP.elapsed, velocity.y * HXP.elapsed, "walls"
+            );
+        }
         super.update();
     }
 
     override public function render(camera:Camera) {
         if(hook != null && hook.isAttached) {
+            Draw.color = 0xFFFFFF;
             Draw.line(centerX, centerY, hook.centerX, hook.centerY);
+            Draw.color = 0x00FF00;
+            Draw.circle(
+                hook.centerX,
+                hook.centerY,
+                MathUtil.distance(
+                    centerX, centerY, hook.centerX, hook.centerY
+                ),
+                100
+            );
         }
         super.render(camera);
     }
